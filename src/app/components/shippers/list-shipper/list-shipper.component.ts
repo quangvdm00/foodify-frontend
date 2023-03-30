a12c0cda8dd763e0eab3d527ba683da4db329e53
Nguyễn Đức Đạt <ducdat612001@gmail.com>
2023-03-30 16:10:06 +0700

    update shipper list - add delete feature to list shipper page

--- src/app/components/shippers/list-shipper/list-shipper.component.ts
+++ src/app/components/shippers/list-shipper/list-shipper.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 import { Router } from '@angular/router';
-import { BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { ShipperService } from 'src/app/shared/service/shipper.service';
 import { Shipper } from 'src/app/shared/tables/shipper';
 
@@ -16,6 +16,9 @@ export class ListShipperComponent {
   thePageNumber = 1;
   thePageSize = 10;
   theTotalElements = 0;
+
+  modalRef: BsModalRef;
+  shipperId: number
 
   constructor(private shipperService: ShipperService,
     private modalService: BsModalService,
@@ -37,6 +40,30 @@ export class ListShipperComponent {
       this.thePageSize = data.page.pageSize;
       this.theTotalElements = data.page.totalElements;
     }
+  }
+
+  openModal(template: TemplateRef<any>, id: number) {
+    this.shipperId = id;
+    console.log(this.shipperId);
+    this.modalRef = this.modalService.show(template, { class: "modal-sm" });
+  }
+
+  confirmBox(shipperId: number, template: TemplateRef<any>) {
+    this.shipperService.deleteShipperById(shipperId).subscribe(res => {
+      this.listAllShipper();
+    });
+    this.modalRef.hide();
+    this.modalRef = this.modalService.show(template, { class: "modal-sm" });
+  }
+
+  decline(): void {
+    this.modalRef.hide();
+  }
+
+  successDelete() {
+    this.modalRef.hide();
+    this.listAllShipper();
+    this.router.navigate(["shippers/list"]);
   }
 }
 

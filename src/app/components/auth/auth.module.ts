f333c723dd7f02bc361c43745b16977c1d94d69b
Nguyễn Đức Đạt <ducdat612001@gmail.com>
2023-04-05 15:16:29 +0700

    create signup component

--- src/app/components/auth/auth.module.ts
+++ src/app/components/auth/auth.module.ts
@@ -4,21 +4,24 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login/login.component';
 
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CarouselModule } from 'ngx-owl-carousel-o';
 import { SharedModule } from '../../shared/shared.module';
+import { SignupComponent } from './signup/signup.component';
+
 
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [LoginComponent, SignupComponent],
   imports: [
     CommonModule,
     AuthRoutingModule,
     ReactiveFormsModule,
     CarouselModule,
     SharedModule,
-    NgbModule
+    NgbModule,
+    FormsModule,
   ]
 })
 export class AuthModule { }
